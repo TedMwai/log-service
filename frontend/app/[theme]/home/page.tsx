79de1ecfde331fd88beb637af1e6a10b85a901d2
Ted Mwai <teddymwai11@gmail.com>
2024-02-04 15:10:27 +0300

    Refactor home page component to fetch logs

--- frontend/app/[theme]/home/page.tsx
+++ frontend/app/[theme]/home/page.tsx
@@ -1,35 +1,25 @@
-import { promises as fs } from "fs"
-import path from "path"
-import { Metadata } from "next"
-import Image from "next/image"
-import { Activity, Bug, Timer } from "lucide-react"
-import { z } from "zod"
+import { Metadata } from "next";
+import { Activity, Bug, Timer } from "lucide-react";
 
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
-import { Tabs, TabsContent } from "@/components/ui/tabs"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
-import { columns } from "./components/columns"
-import { DataTable } from "./components/data-table"
-import { taskSchema } from "./data/schema"
+import { columns } from "./components/columns";
+import { DataTable } from "./components/data-table";
+import { LogSchema } from "./data/schema";
+
 
 export const metadata: Metadata = {
   title: "Dashboard",
   description: "Dashboard & log tracker built using Tanstack Table.",
 }
 
-// Simulate a database read for tasks.
-async function getTasks() {
-  const data = await fs.readFile(
-    path.join(process.cwd(), "app/[theme]/home/data/tasks.json")
-  )
-
-  const tasks = JSON.parse(data.toString())
-
-  return z.array(taskSchema).parse(tasks)
+async function getLogs(): Promise<LogSchema[]> {
+  const response = await fetch("http://localhost:8080/logs")
+  return response.json()
 }
 
 export default async function HomePage() {
-  const tasks = await getTasks()
+  const logs = await getLogs()
 
   return (
     <>
@@ -79,7 +69,7 @@ export default async function HomePage() {
                 </h2>
               </div>
             </div>
-            <DataTable data={tasks} columns={columns} />
+            <DataTable data={logs} columns={columns} />
           </div>
         </div>
       </div>
